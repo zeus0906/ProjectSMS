89d99bd12de4dade9767f955b2f863b6d0de8f4b
zeus0906 <frankynyambi20@gmail.com>
2023-10-16 03:05:29 +0200

    First commit ProjectSMS

---
+++ src/main/java/com/example/pojectsms/PojectSmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pojectsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojectSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PojectSmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pojectsms/PojectSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pojectsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojectSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
