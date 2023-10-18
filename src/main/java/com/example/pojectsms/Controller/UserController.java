97a9e1701ef5e368f0f410d10fa47f7e3a11be4c
zeus0906 <frankynyambi20@gmail.com>
2023-10-18 13:53:28 +0200

    Commit 3

--- src/main/java/com/example/pojectsms/Controller/UserController.java
+++ src/main/java/com/example/pojectsms/Controller/UserController.java
@@ -1,14 +1,20 @@
 package com.example.pojectsms.Controller;
 
+import com.example.pojectsms.Entite.User;
 import com.example.pojectsms.Model.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
     @Autowired
     UserDao userDao;
-
-
+    @GetMapping("/api/Users/all ")
+    public List<User> listeUser(){
+        return userDao.findAll();
+    }
 }
